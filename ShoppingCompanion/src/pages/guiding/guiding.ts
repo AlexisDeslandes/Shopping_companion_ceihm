1900b04fb4dced259f3a5cf32fc2f50440fc75f0
NastyJoke <renaud.costa84@gmail.com>
2019-01-20 18:20:20 +0100

    Coordinates

--- ShoppingCompanion/src/pages/guiding/guiding.ts
+++ ShoppingCompanion/src/pages/guiding/guiding.ts
@@ -26,6 +26,9 @@ export class GuidingPage {
 
     private canvasWidth;
     private canvasHeight;
+    private coordinates;
+    private aisleWidth;
+    private aisleHeight;
 
     constructor(public navCtrl: NavController, public navParams: NavParams,
                 private loading_ctrl: LoadingController, platform: Platform) {
@@ -51,29 +54,67 @@ export class GuidingPage {
         this._CANVAS.width = this.canvasWidth;
         this._CANVAS.height = this.canvasHeight;
 
-        this.initializeCanvas(this.canvasWidth, this.canvasHeight);
+        this.aisleWidth = this._CANVAS.width/3;
+        this.aisleHeight = this._CANVAS.height/10;
 
-        const aisleWidth = this._CANVAS.width/3;
-        const aisleHeight = this._CANVAS.height/10;
         const aisleHorizontalOffset = this._CANVAS.width/9;
         const aisleVerticalOffset = this._CANVAS.height/12;
 
+        this.coordinates = {1: [aisleHorizontalOffset, aisleVerticalOffset], 2: [aisleHorizontalOffset*2 + this.aisleWidth, aisleVerticalOffset],
+            3: [aisleHorizontalOffset, aisleVerticalOffset*2 + this.aisleHeight], 4: [aisleHorizontalOffset*2 + this.aisleWidth, aisleVerticalOffset*2 + this.aisleHeight],
+            5: [aisleHorizontalOffset, aisleVerticalOffset*3 + this.aisleHeight*2], 6: [aisleHorizontalOffset*2 + this.aisleWidth, aisleVerticalOffset*3 + this.aisleHeight*2],
+            7: [aisleHorizontalOffset, aisleVerticalOffset*4 + this.aisleHeight*3], 8: [aisleHorizontalOffset*2 + this.aisleWidth, aisleVerticalOffset*4 + this.aisleHeight*3],
+            9: [aisleHorizontalOffset, aisleVerticalOffset*5 + this.aisleHeight*4], 10: [aisleHorizontalOffset*2 + this.aisleWidth, aisleVerticalOffset*5 + this.aisleHeight*4]}
+
+
+
+        this.initializeCanvas(this.canvasWidth, this.canvasHeight);
+
+        this.drawShop();
+
+    }
+
+    drawPath(start, end) {
+        let startAisle = start.split(':')[0];
+        let startSide = start.split(':')[1];
+        let endAisle = end.split(':')[0];
+        let endSide = end.split(':')[1];
+
+        console.log(startAisle, startSide, endAisle, endSide);
+
+        this.drawLine(this.coordinates[startAisle], this.coordinates[endAisle]);
+
+    }
+
+    drawLine(a, b)
+    {
+        this._CONTEXT.beginPath();
+        this._CONTEXT.moveTo(a[0], a[1]);
+        this._CONTEXT.lineTo(b[0], b[1]);
+        this._CONTEXT.lineWidth = 1;
+        this._CONTEXT.strokeStyle = '#005af1';
+        this._CONTEXT.stroke();
+    }
+
+    drawShop() {
+
         // This is not a loop since the aisles are supposed to be placed manually for actual stores (irregular layouts)
-        this.drawRect(aisleHorizontalOffset, aisleVerticalOffset, aisleWidth, aisleHeight, "#000", "#717171");
-        this.drawRect(aisleHorizontalOffset*2 + aisleWidth, aisleVerticalOffset, aisleWidth, aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[1][0], this.coordinates[1][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[2][0], this.coordinates[2][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
 
-        this.drawRect(aisleHorizontalOffset, aisleVerticalOffset*2 + aisleHeight, aisleWidth, aisleHeight, "#000", "#717171");
-        this.drawRect(aisleHorizontalOffset*2 + aisleWidth, aisleVerticalOffset*2 + aisleHeight, aisleWidth, aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[3][0], this.coordinates[3][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[4][0], this.coordinates[4][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
 
-        this.drawRect(aisleHorizontalOffset, aisleVerticalOffset*3 + aisleHeight*2, aisleWidth, aisleHeight, "#000", "#717171");
-        this.drawRect(aisleHorizontalOffset*2 + aisleWidth, aisleVerticalOffset*3 + aisleHeight*2, aisleWidth, aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[5][0], this.coordinates[5][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[6][0], this.coordinates[6][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
 
-        this.drawRect(aisleHorizontalOffset, aisleVerticalOffset*4 + aisleHeight*3, aisleWidth, aisleHeight, "#000", "#717171");
-        this.drawRect(aisleHorizontalOffset*2 + aisleWidth, aisleVerticalOffset*4 + aisleHeight*3, aisleWidth, aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[7][0], this.coordinates[7][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[8][0], this.coordinates[8][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
 
-        this.drawRect(aisleHorizontalOffset, aisleVerticalOffset*5 + aisleHeight*4, aisleWidth, aisleHeight, "#000", "#717171");
-        this.drawRect(aisleHorizontalOffset*2 + aisleWidth, aisleVerticalOffset*5 + aisleHeight*4, aisleWidth, aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[9][0], this.coordinates[9][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
+        this.drawRect(this.coordinates[10][0], this.coordinates[10][1], this.aisleWidth, this.aisleHeight, "#000", "#717171");
 
+        this.drawPath("1:N", "5:S");
 
     }
 
