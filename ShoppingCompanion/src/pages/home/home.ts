410123bb91f27f49a06921868f37a18a8b1f1408
AlexisDeslandes <alexis.deslandes@alithya.com>
2019-01-28 10:34:31 +0100

    Fix access to shopping list when there is no list

--- ShoppingCompanion/src/pages/home/home.ts
+++ ShoppingCompanion/src/pages/home/home.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {AddShoppingListPage} from "../add-shopping-list/add-shopping-list";
 import {Storage} from "@ionic/storage";
 import {Shopping} from "../../interface/Shopping";
 import {ConsultShoppingListPage} from "../consult-shopping-list/consult-shopping-list";
+import {ShoppingList} from "../../interface/ShoppingList";
 
 @Component({
   selector: 'page-home',
@@ -11,11 +12,11 @@ import {ConsultShoppingListPage} from "../consult-shopping-list/consult-shopping
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private storage : Storage) {
+  constructor(public navCtrl: NavController, private storage: Storage) {
 
   }
 
-  async ionViewDidLoad(){
+  async ionViewDidLoad() {
 
   }
 
@@ -24,6 +25,10 @@ export class HomePage {
   }
 
   async navigate_to_shopping_list() {
-    await this.navCtrl.push(ConsultShoppingListPage);
+    if (new ShoppingList(await this.storage.get("shopping")).shopping.length !== 0) {
+      await this.navCtrl.push(ConsultShoppingListPage);
+    } else {
+      alert("Veuillez créer au moins une liste au préalable.");
+    }
   }
 }
